959f2b47b2fa99de75e2504b676dcbbb928360e5
NMTuan <nmtuan@qq.com>
2022-11-21 13:30:46 +0800

    fix: 简化删除操作，绕过多dialog页面滚动条问题

--- src/content-scripts/components/create.vue
+++ src/content-scripts/components/create.vue
@@ -2,13 +2,13 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-08 11:54:19
- * @LastEditTime: 2022-11-18 17:28:27
+ * @LastEditTime: 2022-11-21 13:30:03
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezBookmarks2\src\content-scripts\components\create.vue
 -->
 <template>
-    <BaseDialog :show="show" title="New Bookmark">
+    <BaseDialog :show="show" @update:show="handleClose" title="New Bookmark">
         <form action="" @submit.prevent="handleSubmit">
             <BaseInput v-model="formData.name" placeholder="Site Title" />
             <BaseInput v-model="formData.url" disabled placeholder="Site Url" />
@@ -18,30 +18,15 @@
                     {{ formData.id ? 'Update' : 'Create' }}
                 </BaseButton>
                 <BaseButton v-if="formData.id" class=":uno: ml-3 bg-red-500/50 hover:bg-red-800" type="button"
-                    @click="showDeleteDialog = true">
+                    @click="handleDelete">
                     <IconDeleteBin5Line />
                 </BaseButton>
             </div>
-            <BaseDialog :show="showDeleteDialog" title="Waring">
-                <p>Data cannot be recovered after deletion. Are you sure you want to delete
-                    <span class=":uno: text-sky-500">[ {{ formData.name }} ]</span> ?
-                </p>
-                <template #foot="{ close }">
-                    <div class="flex justify-end mt-8">
-                        <BaseButton class=":uno: hover:bg-neutral-500/30" @click="close" type="button">Cancel
-                        </BaseButton>
-                        <BaseButton class=":uno: ml-3 bg-red-500/50 hover:bg-red-800" @click="handleDelete"
-                            type="button" :loading="deleteLoading">Delete
-                        </BaseButton>
-                    </div>
-                </template>
-
-            </BaseDialog>
         </form>
     </BaseDialog>
 </template>
 <script setup>
-import { ref, computed, onMounted, watch, nextTick } from 'vue'
+import { ref, computed, watch, nextTick } from 'vue'
 import log from "@/utils/log";
 
 import BaseDialog from './baseDialog.vue';
@@ -65,10 +50,8 @@ const formData = ref({
 const tags = ref('')
 const deleteTagsId = ref([])    // 要移除的tagsId
 const loading = ref(false)
-const deleteLoading = ref(false)
 const el = ref()
 const showState = computed(() => props.show)
-const showDeleteDialog = ref(false)
 
 // 处理 输入框中的 tags
 const formatedTags = computed(() => {
@@ -95,6 +78,10 @@ const formatedTags = computed(() => {
             return total
         }, [])
 })
+
+const handleClose = () => {
+    emits('update:show', false)
+}
 
 const handleSubmit = () => {
     if (loading.value) {
@@ -144,10 +131,11 @@ const handleSubmit = () => {
 }
 
 const handleDelete = () => {
-    if (deleteLoading.value) {
+    const action = confirm(`Data cannot be recovered after deletion. Are you sure you want to delete [${formData.value.name}]`)
+    if (!action) {
         return
     }
-    deleteLoading.value = true
+
     chrome.runtime.sendMessage({
         type: 'deleteBookmark',
         payload: {
@@ -156,8 +144,6 @@ const handleDelete = () => {
     }, ({ errors, data }) => {
         log('[errors]', errors);
         log('[data]', data);
-        deleteLoading.value = false
-        showDeleteDialog.value = false
         emits('update:show', false)
         formData.value = { id: '', name: '', url: '' }
         tags.value = ''
@@ -165,12 +151,8 @@ const handleDelete = () => {
     })
 }
 
-// onMounted(() => {
-//     formData.value.name = document.title
-//     formData.value.url = location.href
-// })
-
 watch(showState, (val) => {
+    log('xxx', val)
     if (!val) {
         return
     }
