32ed7e3ed33edd28eb8cbdcb193874e951da4cff
nmtuan <nmtuan@qq.com>
2022-12-13 06:55:33 +0800

    chore: 更新依赖 unocss 转义问题修复

--- src/create/App.vue
+++ src/create/App.vue
@@ -2,13 +2,13 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 14:09:25
- * @LastEditTime: 2022-12-12 17:04:00
+ * @LastEditTime: 2022-12-13 06:53:52
  * @LastEditors: NMTuan
  * @Description: 
- * @FilePath: \ezBookmarks2\src\create\Create.vue
+ * @FilePath: \ezBookmarks2\src\create\App.vue
 -->
 <template>
-    <div class="text-xl font-bold w-full h-full  bg-purple-400">
+    <div class="text-xl font-bold w-full h-full  bg-#a8c9c4">
         <div>
             testpage
             <div>
