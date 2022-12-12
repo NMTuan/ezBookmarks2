d90023c3798e7076f9233d17b936e780341f2324
NMTuan <nmtuan@qq.com>
2022-12-12 16:05:58 +0800

    feat: 增加多语言

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,10 +2,10 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 11:07:04
- * @LastEditTime: 2022-12-02 11:42:00
+ * @LastEditTime: 2022-12-12 16:04:54
  * @LastEditors: NMTuan
  * @Description: 
- * @FilePath: \base_vite_chrome_extension\src\options\App.vue
+ * @FilePath: \ezBookmarks2\src\options\App.vue
 -->
 <template>
     <div class="bg-red-200 py-12">
@@ -17,3 +17,6 @@
         </div>
     </div>
 </template>
+<script setup>
+console.log('[i18n]',chrome.i18n.getMessage("ext_name"))
+</script>
