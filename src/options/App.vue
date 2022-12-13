1789668c1a779174e79a0b66ee66c65579c49cdd
nmtuan <nmtuan@qq.com>
2022-12-14 07:34:24 +0800

    chore: 添加表单

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 11:07:04
- * @LastEditTime: 2022-12-13 16:45:40
+ * @LastEditTime: 2022-12-14 06:50:07
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezBookmarks2\src\options\App.vue
@@ -10,6 +10,7 @@
 <template>
     <div>
         layout
+        <router-link :to="{ name: 'index' }">[index]</router-link>
         <router-link :to="{ name: 'login' }">[login]</router-link>
         <router-link :to="{ name: 'create' }">[create]</router-link>
         <hr>
@@ -24,7 +25,8 @@
 console.log('[i18n]', chrome.i18n.getMessage("ext_name"))
 </script>
 <style>
-html,body {
+html,
+body {
     @apply text-base;
 }
 </style>
--- src/options/router/create.vue
+++ src/options/router/create.vue
@@ -2,13 +2,67 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-12-05 14:27:29
- * @LastEditTime: 2022-12-09 16:38:37
+ * @LastEditTime: 2022-12-14 07:32:57
  * @LastEditors: NMTuan
  * @Description: 
- * @FilePath: \ezBookmarks2\src\dialog\router\create.vue
+ * @FilePath: \ezBookmarks2\src\options\router\create.vue
 -->
 <template>
     <div>
-        create
+        <div>create</div>
+        <p>loading: {{ loading }}</p>
+        <p>title: <input type="text" v-model="formData.title"></p>
+        <p>url: <input type="text" v-model="formData.url"></p>
+        <p>tags: <input type="text" v-model="tags"></p>
+        <p><button @click="submit">submit</button></p>
+        <pre>formData: {{ formData }}</pre>
+        <pre>tags: {{ tags }}</pre>
+        <pre>submitRes: {{ submitRes }}</pre>
     </div>
 </template>
+<script setup>
+import { ref, watchEffect, inject } from 'vue'
+
+const db = inject('db')
+const loading = ref(false)
+const formData = ref({})
+const tags = ref('')
+const submitRes = ref({})
+
+// 处理 tags。 过滤长空格；分割字符串；去重；
+watchEffect(() => {
+    formData.value.tags = tags.value.replace(/\s/g, ' ').split(/[,， ]/).reduce((total, tag) => {
+        // 存在且不重复
+        if (tag && total.indexOf(tag) < 0) {
+            total.push(tag)
+        }
+        return total
+    }, [])
+})
+
+const submit = () => {
+    if (formData.value.title === '' || formData.value.url === '') {
+        return
+    }
+    loading.value = true
+    db.post(formData.value)
+        .then(res => {
+            loading.value = false
+            console.log('[res]', res)
+            submitRes.value = res
+            formData.value = {}
+            tags.value = ''
+        })
+        .catch((err) => {
+            loading.value = false
+            console.error('[err]', err)
+        })
+
+}
+</script>
+<style scoped lang="scss">
+input,
+button {
+    @apply border;
+}
+</style>
