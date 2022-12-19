a6d0af3ac851d9e188352537ec59f3e6a5bc0621
NMTuan <nmtuan@qq.com>
2022-12-19 10:24:08 +0800

    feat: menu style

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 11:07:04
- * @LastEditTime: 2022-12-19 08:11:15
+ * @LastEditTime: 2022-12-19 10:20:03
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezBookmarks2\src\options\App.vue
@@ -13,23 +13,22 @@
         <div class="fixed z-10 h-full overflow-hidden" :class="menuClass">
             <div class="w-64 h-full">
                 <simplebar class="simplebar h-full">
-                    <router-link :to="{ name: 'index' }">[index]</router-link>
-                    <router-link :to="{ name: 'login' }">[login]</router-link>
-                    <router-link :to="{ name: 'create' }">[create]</router-link>
-                    <router-link :to="{ name: 'tags' }">[tags]</router-link>
-                    <router-link :to="{ name: 'setting' }">[setting]</router-link>
-                    <router-link :to="{ name: 'import' }">[import]</router-link>
-                    <p v-for="i in 100">{{ i }}</p>
+                    <LayoutMenu></LayoutMenu>
                 </simplebar>
             </div>
         </div>
         <!-- main -->
         <div class="flex-1 h-full overflow-hidden" :class="mainClass">
             <simplebar class="simplebar h-full">
-                <div class="flex items-center justify-between p-4 leading-none bg-warm-gray-50/50 backdrop-blur fixed left-0 top-0 right-0 z-10"
-                    :class="mainClass">
+                <div
+                    class="flex items-center justify-between p-4 leading-none bg-cool-gray-50/50 backdrop-blur fixed left-0 top-0 right-0 z-10"
+                    :class="mainClass"
+                >
                     <div class="flex items-center">
-                        <div @click="togglePin" class="i-ri-menu-line text-xl"></div>
+                        <div
+                            @click="togglePin"
+                            class="i-ri-menu-line text-xl"
+                        ></div>
                         <div class="ml-4">简单书签</div>
                     </div>
                     <div>
@@ -48,6 +47,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import { pin, togglePin } from './components/LayoutPin'
+import LayoutMenu from './components/LayoutMenu.vue'
 import simplebar from 'simplebar-vue'
 import 'simplebar/dist/simplebar.min.css'
 console.log('[i18n]', chrome.i18n.getMessage('ext_name'))
@@ -57,16 +57,16 @@ const menuClass = computed(() => {
     switch (pin.value) {
         case true:
             classNames.push(
-                'w-0 bg-warm-gray-100/90 border-r-8 border-warm-gray-300/50 transition-all duration-500'
+                'w-0 bg-cool-gray-100/90 border-r-8 border-cool-gray-300/50 transition-all duration-500'
             )
             classNames.push('hover:w-64 hover:border-r')
             break
         case false:
             classNames.push(
-                'w-0 bg-warm-gray-100/90 border-r-8 border-warm-gray-300/50 transition-all duration-500'
+                'w-0 bg-cool-gray-100/90 border-r-8 border-cool-gray-300/50 transition-all duration-500'
             )
             classNames.push('hover:w-64 hover:border-r')
-            classNames.push('lg:w-64 lg:bg-warm-gray-200/50 lg:!border-none')
+            classNames.push('lg:w-64 lg:bg-cool-gray-200/50 lg:!border-none')
     }
     return classNames
 })
@@ -87,7 +87,7 @@ const mainClass = computed(() => {
 html,
 body,
 #app {
-    @apply text-base bg-warm-gray-50 h-full overflow-hidden;
+    @apply text-base bg-cool-gray-50 h-full overflow-hidden;
 }
 
 .simplebar {
@@ -99,7 +99,7 @@ body,
 
     .simplebar-scrollbar {
         &:before {
-            @apply bg-warm-gray-400;
+            @apply bg-cool-gray-400;
         }
     }
 }
---
+++ src/options/components/LayoutMenu.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2022-12-19 09:34:10
+ * @LastEditTime: 2022-12-19 10:23:32
+ * @LastEditors: NMTuan
+ * @Description: 
+ * @FilePath: \ezBookmarks2\src\options\components\LayoutMenu.vue
+-->
+<template>
+    <div class="menu">
+        <div class="title">Menu</div>
+        <router-link class="link" :to="{ name: 'index' }"> index </router-link>
+        <router-link class="link" :to="{ name: 'login' }"> login </router-link>
+        <div class="title">base type</div>
+        <router-link class="link" :to="{ name: 'create' }">
+            create
+        </router-link>
+        <router-link class="link" :to="{ name: 'tags' }"> tags </router-link>
+        <div class="title">sync type</div>
+        <router-link class="link" :to="{ name: 'setting' }">
+            setting
+        </router-link>
+        <div class="title">cloud type</div>
+        <router-link class="link" :to="{ name: 'import' }">
+            import
+        </router-link>
+
+        <router-link class="link" :to="{ name: 'index' }"> index</router-link>
+        <p v-for="i in 100">{{ i }}</p>
+    </div>
+</template>
+<style scoped lang="scss">
+.menu {
+    @apply leading-none;
+}
+.title {
+    @apply px-4 pt-5 pb-2;
+    @apply text-cool-gray-400 capitalize text-sm;
+}
+.link {
+    @apply block p-4;
+    @apply capitalize font-bold text-cool-gray-600;
+    @apply hover:bg-cool-gray-200;
+}
+.router-link-active,
+.router-link-active:hover {
+    // @apply bg-cool-gray-400;
+    @apply bg-blue-400;
+    @apply text-white;
+}
+</style>
