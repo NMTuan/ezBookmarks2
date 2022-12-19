1b660f9a8cd3fdfe240a04bdadf80b5625f06e30
nmtuan <nmtuan@qq.com>
2022-12-19 08:10:11 +0800

    chore: 头部 fixed

--- src/options/App.vue
+++ src/options/App.vue
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-11-22 11:07:04
- * @LastEditTime: 2022-12-16 16:57:36
+ * @LastEditTime: 2022-12-19 08:08:00
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezBookmarks2\src\options\App.vue
@@ -10,38 +10,26 @@
 <template>
     <div class="h-full flex">
         <!-- menu -->
-        <div
-            class="fixed z-10 h-full overflow-hidden transition-all duration-500"
-            :class="menuClass"
-        >
+        <div class="fixed z-10 h-full overflow-hidden transition-all duration-500" :class="menuClass">
             <div class="w-64 h-full">
                 <simplebar class="simplebar h-full">
                     <router-link :to="{ name: 'index' }">[index]</router-link>
                     <router-link :to="{ name: 'login' }">[login]</router-link>
                     <router-link :to="{ name: 'create' }">[create]</router-link>
                     <router-link :to="{ name: 'tags' }">[tags]</router-link>
-                    <router-link :to="{ name: 'setting' }"
-                        >[setting]</router-link
-                    >
+                    <router-link :to="{ name: 'setting' }">[setting]</router-link>
                     <router-link :to="{ name: 'import' }">[import]</router-link>
                     <p v-for="i in 100">{{ i }}</p>
                 </simplebar>
             </div>
         </div>
         <!-- main -->
-        <div
-            class="flex-1 h-full transition-all duration-500 overflow-hidden"
-            :class="mainClass"
-        >
+        <div class="flex-1 h-full transition-all duration-500 overflow-hidden" :class="mainClass">
             <simplebar class="simplebar h-full">
-                <div
-                    class="flex items-center justify-between p-4 leading-none bg-warm-gray-50/50 backdrop-blur fixed z-10"
-                >
+                <div class="flex items-center justify-between p-4 leading-none bg-warm-gray-50/50 backdrop-blur fixed left-0 top-0 right-0 z-10"
+                    :class="mainClass">
                     <div class="flex items-center">
-                        <div
-                            @click="togglePin"
-                            class="i-ri-menu-line text-xl"
-                        ></div>
+                        <div @click="togglePin" class="i-ri-menu-line text-xl"></div>
                         <div class="ml-4">简单书签</div>
                     </div>
                     <div>
@@ -101,12 +89,14 @@ body,
 #app {
     @apply text-base bg-warm-gray-50 h-full overflow-hidden;
 }
+
 .simplebar {
     &:hover {
         .simplebar-track {
             @apply bg-black/5;
         }
     }
+
     .simplebar-scrollbar {
         &:before {
             @apply bg-warm-gray-400;
