a68891de4d400d9b549dcd037d393984bcccfa91
nmtuan <nmtuan@qq.com>
2022-12-20 08:28:06 +0800

    chore(base): 准备写随机漫步

--- src/options/router/base.vue
+++ src/options/router/base.vue
@@ -2,16 +2,13 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-12-19 13:49:44
- * @LastEditTime: 2022-12-19 15:46:18
+ * @LastEditTime: 2022-12-20 08:27:40
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezBookmarks2\src\options\router\base.vue
 -->
 <template>
-    <div>
-        <div>base</div>
-        <RouterView></RouterView>
-    </div>
+    <RouterView></RouterView>
 </template>
 <script>
 export default {
---
+++ src/options/router/base/random.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2022-12-20 08:16:38
+ * @LastEditTime: 2022-12-20 08:26:12
+ * @LastEditors: NMTuan
+ * @Description: 
+ * @FilePath: \ezBookmarks2\src\options\router\base\random.vue
+-->
+<template>
+    <div>
+        123
+    </div>
+</template>
+<script setup>
+
+</script>
+<script>
+export default {
+    icon: 'i-ri-shuffle-line'
+}
+</script>
